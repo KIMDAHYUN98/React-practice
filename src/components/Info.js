a7299484811509a70a5fa791d88e18d9b6b19720
kdh98 <dbkdh98@gmail.com>
2025-12-26 13:57:03 +0900

    ✨8장 Hooks : 8.1 useState

---
+++ src/components/Info.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export default function Info() {
+  const [name, setName] = useState('');
+  const [nickname, setNickname] = useState('');
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+
+  return (
+    <>
+      <div>
+        <input type='text' value={name} onChange={onChangeName}/>
+        <input type='text' value={nickname} onChange={onChangeNickname}/>
+      </div>
+      <div>
+        <b>이름 : </b> {name}
+      </div>
+      <div>
+        <b>닉네임 : </b> {nickname}
+      </div>
+    </>
+  );
+};
