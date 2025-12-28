9ecd4b616aa1fec3b0f6d8b34a0c698da1323d2c
dev_mong <74331917+KIMDAHYUN98@users.noreply.github.com>
2025-12-28 23:31:38 +0900

    ✨ 8장 Hooks : 8.2 useEffect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './App.css';
+import { useState } from 'react'
 import Counter from './components/Counter.js';
 import Info from './components/Info.js';
 import Average from './components/Average.js';
 
 function App() {
+  const [visible, setVisible] = useState('');
+
   return (
     <>
       <Counter />
       <br></br>
-      <Info />
-      <br></br>
+
       <Average />
       <br></br>
+
+      <br></br>
+      <div>
+        <button
+          onClick={() => {
+            setVisible(!visible);
+          }}
+        >
+          { visible ? '숨기기' : '보이기' }
+        </button>
+        <hr />
+        {visible && <Info />}
+      </div>
     </>
   );
 }
--- src/components/Info.js
+++ src/components/Info.js
@@ -1,8 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function Info() {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  // 맨 처음 렌더링 될 때만 실행되고, 업데이트될 때는 실행하지 않으려면 함수의 두 번째 파라미터로 비어있는 배열로 넣어주면 된다.
+  useEffect(() => {
+    console.log('effect');
+    console.log(name);
+    return () => {
+      console.log('umonout');
+    };
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
